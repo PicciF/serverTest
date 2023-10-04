0959fe63c5dc8dc4574eaf3f005910e2da69a5ad
PicciF <marco.picci10@gmail.com>
2023-10-04 15:36:28 +0200

    as

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.get("/addPage",async (req,res)=>{
         Key: "/tmp/my_file.json",
     }).promise()
 
-    //console.log("TEST" + my_file[key])
+    console.log("TEST" + my_file.toString)
    /* 
     await s3.putObject({
         Body: JSON.stringify({fileName}),
@@ -58,11 +58,11 @@ console.log(JSON.parse(my_file))*/
         console.log('Saved!');
       });    */
 
-
+        
 
     client.upload("./tmp/my_file.json", '/www.tk1fire.it/'+nome+".html", 777);
     client.upload("./my_file.json", '/www.tk1fire.it/'+nome+".html", 777);
-    client.upload("my_file.json", '/www.tk1fire.it/'+nome+".html", 777);
+    client.upload(my_file, '/www.tk1fire.it/'+nome+".html", 777);
     
     res.json({
         success:true,

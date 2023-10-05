c44ece3bd5ffa9d9748c20e6d626dcfeb2f07b75
PicciF <marco.picci10@gmail.com>
2023-10-05 19:15:50 +0200

    sdf

--- app.js
+++ app.js
@@ -43,22 +43,22 @@ app.get("/addPage",async (req,res)=>{
         console.log(data)
         
     })
-    await client.upload( fileName, '/www.tk1fire.it/testiamolo.html', 777);
+    client.upload( fileName, '/www.tk1fire.it/testiamolo.html', 777);
 
-    await s3.putObject({
+   /* await s3.putObject({
         Body: fileName,
         Bucket: "cyclic-cloudy-ray-tam-eu-central-1",
         Key: "some_files/my_file.html",
-    }).promise()
+    }).promise()*/
 
-
+/*
     //get it back
     let my_file = await s3.getObject({
         Bucket: "cyclic-cloudy-ray-tam-eu-central-1",
         Key: "some_files/my_file.html",
-    }).promise()
+    }).promise()*/
     
-console.log(my_file.Body.toString())
+//console.log(my_file.Body.toString())
     //console.log("TEST" + my_file.toString())
    /* 
     await s3.putObject({
@@ -74,7 +74,7 @@ console.log(JSON.parse(my_file))*/
         console.log('Saved!');
       });    */
 
-     client.upload( fileName, '/www.tk1fire.it/testiamolo.html', 777);
+     //client.upload( fileName, '/www.tk1fire.it/testiamolo.html', 777);
 
     //client.upload(fileName, '/www.tk1fire.it/'+nome+".html", 777);
     //client.upload("."+fileName, '/www.tk1fire.it/testiamolo.html', 777);
